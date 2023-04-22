c45fb04b05acc3e22e1137017190fe8a14181f83
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-04-22 18:20:30 +0100

    done with the folder strucuture

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Testimonials from "./Testimonials";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Footer from "./Footer";
+
+export {
+  Navbar,
+  Business,
+  CTA,
+  Billing,
+  Hero,
+  Footer,
+  Stats,
+  CardDeal,
+  Testimonials,
+  Clients,
+};
